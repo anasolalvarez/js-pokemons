e774c638b69cf66bde67ab1e0d443df597c613ab
Ana Sol Alvarez <anasol.alv@gmail.com>
2021-06-04 11:50:29 +0200

    Update script.js

--- js-pokemons/js/script.js
+++ js-pokemons/js/script.js
@@ -1,7 +1,7 @@
 var pokemonList = { name1:'Balbasur', name2:'Ivysaur', name3:'Venusaur'};
 
-let Balbasur = [{ type: 'grass-poison', height: 0.7 , color: 'blue'}];
-let Ivysaur = [{type: 'grass-poison', height:1, color:'lightblue'}];
-let Venusaur = [{type:'grass-poison', height:2, color:'lila'}];
+let Balbasur =[{ type: 'grass-poison', height: 0.7 , color: 'blue'}];
+let Ivysaur =[{type: 'grass-poison', height:1, color:'lightblue'}];
+let Venusaur =[{type:'grass-poison', height:2, color:'lila'}];
 
 console.log(pokemonList);
