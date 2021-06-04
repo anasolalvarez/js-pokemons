70d5c0281645f79189b175bbfa11bfe42e0b9835
Ana Sol Alvarez <anasol.alv@gmail.com>
2021-06-04 11:42:52 +0200

    js changes

--- js-pokemons/js/script.js
+++ js-pokemons/js/script.js
@@ -1,17 +1,7 @@
-let pokemonList = ['Balbasur','Ivysaur','Venusaur']
-let Balbasur = {
-  size: big,
-  color: green,
-  food: grass,
-};
-let Ivysaur = {
-  size: medium,
-  color: yellow,
-  food: apples,
-};
-let Venusaur = {
-  size: small,
-  color: lila,
-  food: flowers,
-};
+var pokemonList = { name1:'Balbasur', name2:'Ivysaur', name3:'Venusaur'};
+
+let Balbasur = [{ type: 'grass-poison', height: 0.7 , color: 'blue'}];
+let Ivysaur = [{type: 'grass-poison', height:1, color:'lightblue'}];
+let Venusaur = [{type:'grass-poison', height:2, color:'lila'}];
+
 console.log(pokemonList);
