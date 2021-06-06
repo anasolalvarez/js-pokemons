3efb712862bb05019640370785af4fca81243b10
Ana Sol Alvarez <anasol.alv@gmail.com>
2021-06-06 13:01:01 +0200

    js corrections

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 let pokemonList = [{name:'Bulbasaur', height:0.7, types: ['grass', 'poison']}];
 pokemonList[1]= {name:'Venusaur', height:1, types:['seed', 'overgrown']};
-pokemonList[2]= {name:'', height:2, types:['fire']};
-pokemonList[3]= {name:'pepito', height:3, types:['fire','water']};
+pokemonList[2]= {name:'Charmander', height:2, types:['fire']};
+pokemonList[3]= {name:'Caterpie', height:3, types:['fire','water']};
 
 console.log(pokemonList);
