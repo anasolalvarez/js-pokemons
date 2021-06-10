393657b0d21bc6ce95290d1285f14f05e150fd0a
Ana Sol Alvarez <anasol.alv@gmail.com>
2021-06-10 11:46:42 +0200

    deleted codes

--- js/script.js
+++ js/script.js
@@ -38,17 +38,6 @@ let pokemonList2 = [{
 
 console.log(pokemonList2);
 
-// document.write('Balbasaur, ', 'Venusaur, ', 'Charmander, ', 'Caterpie. ');
-
-
-// let pokemonsList = [] //'Balbasaur', 'Venusaur', 'Charmander','Caterpie'
-// let text = '';
-// let i = 0;
-// for (; pokemonList[i];) {
-//   text = text + '' + pokemonsList[i];
-//   i++;
-// }
-// console.log('Balbasaur height:0.7 - Venusaur height:1 - Charmender height:2 - Caterpie height:0.3');
 
 for (let i = 0; i < pokemonList.lenght; i++) {
   if (pokemonList[i].height > 1.8){
@@ -69,64 +58,3 @@ function printArrayDetails (List) {
 
 printArrayDetails (pokemonList);
 printArrayDetails (pokemonList2);
-
-
-function divide(dividend, divisor){
-  if(divisor === 0){
-    return "You’re trying to divide by zero."
-  }else{
-    let result = dividend / divisor;
-    return result;
-  }
-}
-
-console.log(divide(4, 2));
-console.log(divide(7, 0));
-console.log(divide(1, 4));
-console.log(divide(12, -3));
-
-let x = 5;
-
-function foo(){
-  let y = 10;
-  console.log("From inside foo(): x = " + x);
-  console.log("From inside foo(): y = " + y);
-}
-
-foo();
-console.log("From outside foo(): x = " + x);
-console.log("From outside foo(): y = " + y);
-
-
-
-
-// let Venusaur = [];
-// if (pokemonList[1].height > 1.8) {
-//   console.log('this is a big pokemon, Wow that is big!')
-// } else if (pokemonList[1].height > 0.5 && pokemonList[1].height < 1.8) {
-//   console.log('this is an average pokemon')
-// } else {
-//   console.log('this is a small pokemon')
-// }
-//
-// let Charmander = []; //{name:'Charmander', height: 2}
-// if (pokemonList[2].height > 1.8) {
-//   console.log('this is a big pokemon, Wow that is big!')
-// } else if (pokemonList[2].height > 0.5 && pokemonList[2].height < 1.8) {
-//   console.log('this is an average pokemon')
-// } else {
-//   console.log('this is a small pokemon')
-// }
-//
-// let Caterpie = []; //{name:'Caterpie', height: 0.3}
-// if (pokemonList[3].height > 1.8) {
-//   console.log('this is a big pokemon, Wow that is big!')
-// } else if (pokemonList[3].height > 0.5 && pokemonList[3].height < 1.8) {
-//   console.log('this is an average pokemon')
-// } else {
-//   console.log('this is a small pokemon')
-// }
-
-//let pokemonList2 = [
-// different set of Pokémon objects
-//];
