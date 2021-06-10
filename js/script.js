8eb593566cbb94ed35f6d1e81841850638d47d10
Ana Sol Alvarez <anasol.alv@gmail.com>
2021-06-10 12:24:42 +0200

    for each function

--- js/script.js
+++ js/script.js
@@ -17,44 +17,29 @@ let pokemonList = [{
 }];
 
 console.log(pokemonList);
+//
+// let pokemonList2 = [{
+//   name: 'Kakuna',
+//   height: 0.6,
+//   types: ['flying', 'psychic']
+// }, {
+//   name: 'Blastoise',
+//   height: 1.6,
+//   types: ['grass', 'electric']
+// }, {
+//   name: 'Rattata',
+//   height: 0.3,
+//   types: ['fighting', 'ghost']
+// }, {
+//   name: 'Nidoking',
+//   height: 1.4,
+//   types: ['psychic', 'water']
+// }];
+//
+// console.log(pokemonList2);
 
-let pokemonList2 = [{
-  name: 'Kakuna',
-  height: 0.6,
-  types: ['flying', 'psychic']
-}, {
-  name: 'Blastoise',
-  height: 1.6,
-  types: ['grass', 'electric']
-}, {
-  name: 'Rattata',
-  height: 0.3,
-  types: ['fighting', 'ghost']
-}, {
-  name: 'Nidoking',
-  height: 1.4,
-  types: ['psychic', 'water']
-}];
 
-console.log(pokemonList2);
-
-
-for (let i = 0; i < pokemonList.lenght; i++) {
-  if (pokemonList[i].height > 1.8){
-    document.write('pokemonList[i].name' + 'Wow that is big');
-  } else if (pokemonList.height > 0.5 && pokemonList.height < 1.8){
-    document.write('pokemonList[i].name' +'this is an average pokemon');
-  } else {
-    document.write('pokemonList[i].name' +'this is a small pokemon');
-  }
-};
-
-function printArrayDetails (List) {
-  for (let i = 0; 0 < List.lenght; i++){
-    document.write ('<p>' + List[i].name + '</p>')
-    console.log (List[i].name);
-  }
+function myLoopFunction(pokemonList) {
+  console.log(pokemonList.name + ' is ' + pokemonList.height + ' height.');
 }
-
-printArrayDetails (pokemonList);
-printArrayDetails (pokemonList2);
+pokemonList.forEach(myLoopFunction);
