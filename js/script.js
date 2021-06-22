332d43d7f83f16a2df1fe64f71081bd58d60a04a
Ana Sol Alvarez <anasol.alv@gmail.com>
2021-06-22 11:46:36 +0200

    styling

--- js/script.js
+++ js/script.js
@@ -1,23 +1,5 @@
 var pokemonRepository = (function() {
-  var pokemonList = [
-    // {
-    //   name: 'Bulbasaur',
-    //   height: 0.7,
-    //   types: ['grass', 'poison']
-    // }, {
-    //   name: 'Venusaur',
-    //   height: 1,
-    //   types: ['seed', 'overgrown']
-    // }, {
-    //   name: 'Charmander',
-    //   height: 2,
-    //   types: ['fire']
-    // }, {
-    //   name: 'Caterpie',
-    //   height: 0.3,
-    //   types: ['fire', 'water']
-    // }
-  ];
+  var pokemonList = [];
 
   function getAll() {
     return pokemonList;
@@ -27,9 +9,6 @@ var pokemonRepository = (function() {
     pokemonList.push(item);
   }
 
-  // function loop(item) {
-  //   pokemonRepository.forEach(item)
-  // }
   function addListItem(pokemon) {
     let pokemonList = document.querySelector('.pokemon-list'); //define
     let listPokemon = document.createElement('li'); //create li element
@@ -42,10 +21,6 @@ var pokemonRepository = (function() {
       showDetails(pokemon)
     })
   }
-
-  // function showDetails(pokemon){
-  // console.log(pokemon);
-  // }
 
   let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
 
@@ -109,61 +84,9 @@ function myLoopFunction(pokemonList) {
 myLoopFunction(pokemonRepository.getAll())
 
 
-// pokemonRepository.getAll().forEach(function(pokemon) {
-//   pokemonRepository.addListItem(pokemon);
-// });
-
 pokemonRepository.loadList().then(function() {
   // Now the data is loaded!
   pokemonRepository.getAll().forEach(function(pokemon) {
     pokemonRepository.addListItem(pokemon);
   });
 });
-
-///////////
-
-// fetch('https://pokeapi.co/api/v2/pokemon/').then(function (response) {
-//   return response.json(); // This returns a promise!
-// }).then(function (pokemonList) {
-//   console.log(pokemonList); // The actual JSON response
-// }).catch(function () {
-//   // Error
-// });
-
-///
-
-// let pokemonRepository = (function () {
-//   let pokemonList = [];
-//   let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
-//
-//   // Other functions remain here
-//
-//   function loadList() {
-//     return fetch(apiUrl).then(function (response) {
-//       return response.json();
-//     }).then(function (json) {
-//       json.results.forEach(function (item) {
-//         let pokemon = {
-//           name: item.name,
-//           detailsUrl: item.url
-//         };
-//         add(pokemon);
-//       });
-//     }).catch(function (e) {
-//       console.error(e);
-//     })
-//   }
-//
-//   return {
-//     add: add,
-//     getAll: getAll,
-//     loadList: loadList
-//   };
-// })();
-//
-// pokemonRepository.loadList().then(function() {
-//   // Now the data is loaded!
-//   pokemonRepository.getAll().forEach(function(pokemon){
-//     pokemonRepository.addListItem(pokemon);
-//   });
-// });
